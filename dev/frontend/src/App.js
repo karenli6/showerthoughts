0ec2b168759b522318ac83ef2488004c4e4faec2
trifork3 <rwilliams8183@gmail.com>
2023-01-17 16:16:56 -0500

    cleaned up dev/frontend

--- dev/frontend/src/App.js
+++ dev/frontend/src/App.js
@@ -3,16 +3,11 @@ import ForceGraph3D from 'react-force-graph-3d';
 
 import * as jsonFile from "./data.json";
 
-labels = (node) => {
-    console.log(node);
-    return `(${node.group}) ${node.id}`;
-}
-
 function App() {
     return(
         <div id="root">
             <h1>[ DEV ]</h1>
-            <ForceGraph3D graphData={ jsonFile } nodeLabel={ labels } />
+            <ForceGraph3D graphData={ jsonFile } />
         </div>
     );
 }
